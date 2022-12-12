7bb99a925dc118a2b659bffb471d1c73d281c27b
Radomyr Horban <horban.radomyr@gmail.com>
2022-12-12 16:22:19 +0200

    Wrote tests for axious and App component

--- src/tests/DataService.test.js
+++ src/tests/DataService.test.js
@@ -1,8 +1,24 @@
-describe("Data Service fake Tests", () => {
-  it("just 1st fake test", () => {
-    expect(1).toEqual(2);
-  });
-});
+// describe('Data Service fake Tests', () => {
+//   it('just 1st fake test', () => {
+//     expect(1).toEqual(2);
+//   });
+// });
 
 // TODO: Your test need to be here instead of fake tests
+import mockAxios from 'axios';
+import getGitHubUser from '../services/DataService/index';
 
+describe('testing api requests', () => {
+  // const user = 'yurkovskiy';
+  it('should return user information by specified id', async () => {
+    mockAxios.get.mockImplementationOnce(() =>
+      Promise.resolve({
+        data: {
+          response: 'SomeString',
+        },
+      })
+    );
+    const res = await getGitHubUser();
+    expect(res.data.response).toContain('SomeString');
+  });
+});
--- src/tests/InfoComponent.test.js
+++ src/tests/InfoComponent.test.js
@@ -1,7 +1,18 @@
-describe("just fake tests for Info Component", () => {
-  it("1st fake test of Info Component", () => {
-    expect(true).toEqual(false);
-  });
-});
+// describe('just fake tests for Info Component', () => {
+//   it('1st fake test of Info Component', () => {
+//     expect(true).toEqual(true);
+//   });
+// });
 
 // TODO: Your test need to be here instead of fake tests
+import Info from '../components/Info/index';
+import renderer from 'react-test-renderer';
+
+// TODO: Your test need to be here instead of fake tests
+describe('App Component test', () => {
+  it('renders correctly', () => {
+    const wrapper = renderer.create(<Info />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+    // expect(App).toEqual(true);
+  });
+});
